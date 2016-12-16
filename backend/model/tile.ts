60d3ebbd8d30cd8be4a05154ded27580f8504e29
Jose Miguel Gallas Olmedo <jgallaso@redhat.com>
2016-12-16 13:17:59 +0100

    Leave only files to deploy the frontend

--- backend/model/tile.ts
+++
@@ -1,4 +0,0 @@
-export default class Tile {
-    static x: string = "x";
-    static o: string = "o";
-};
--- backend/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-
-const index: Router = Router();
-
-index.get("/", (req, res) => {
-    res.send("Hello node with TS!");
-});
-
-export default index;
